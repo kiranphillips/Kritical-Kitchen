925aee379f5aea412068813c1afd2221e8f00ce6
Anna Clausen <anna.revall.clausen@gmail.com>
2022-06-02 10:29:44 -0700

    updated javascript

--- index.js
+++ index.js
@@ -12,8 +12,24 @@ function displayRestaurants(restaurants) {
         const image = document.createElement('img')
         image.src = restaurant.image
 
+        image.addEventListener('click', () => {
+            document.querySelector('.info-image').src = restaurant.image
+            document.querySelector('.name').textContent = restaurant.name
+            document.querySelector('.description').textContent = restaurant.description
+            document.querySelector('#thumbs-up-display').textContent = restaurant.likes
+            document.querySelector('#thumbs-down-display').textContent = restaurant.dislikes
+        })
+
         restaurantMenu.appendChild(image)
+        
     })
+}
+
+function showRestaurantInfo(restaurant) {
+    const name = document.getElementsByClassName("name")
+    name.textContent = restaurant.name
+    const image = document.getElementsByClassName("info-image")
+    image.src = restaurant.image
 }
 
 
