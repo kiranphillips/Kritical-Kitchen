7801c664811f7dd5c584f3998fde3ab1b3f0349e
Kiran Phillips <Kiranphillips61@gmail.com>
2022-06-03 08:29:22 -0600

    project nearly done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const init = () => {
     fetch("http://localhost:3000/restaurants")
         .then(response => response.json())
         .then((data) => (displayRestaurants(data)))
+        
 }
 
 function displayRestaurants(restaurants) {
@@ -11,6 +12,9 @@ function displayRestaurants(restaurants) {
     restaurants.forEach(restaurant => {
         const image = document.createElement('img')
         image.src = restaurant.image
+        const div = document.createElement("div")
+            div.classList.add("mySlides")
+            div.classList.add("fade")
 
         image.addEventListener('click', () => {
             document.querySelector('.info-image').src = restaurant.image
@@ -19,10 +23,25 @@ function displayRestaurants(restaurants) {
             document.querySelector('#thumbs-up-display').textContent = restaurant.likes
             document.querySelector('#thumbs-down-display').textContent = restaurant.dislikes
         })
-
-        restaurantMenu.appendChild(image)
-        
+        div.append(image)
+        restaurantMenu.appendChild(div)
     })
+    showSlides();
+}
+let slideIndex = 0;
+
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName('mySlides');
+         console.log(slides)
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 2500); // Change image every 2 seconds
 }
 
 function showRestaurantInfo(restaurant) {
@@ -32,5 +51,11 @@ function showRestaurantInfo(restaurant) {
     image.src = restaurant.image
 }
 
+function logSubmit(event){
+    event.preventDefault();
+console.log(logSubmit)
+    const form = document.getElementById('form');
 
+form.addEventListener('submit', logSubmit);
+}
 document.addEventListener("DOMContentLoaded", init)
